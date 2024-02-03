81fadd701c8d31ece4fba3f612fe7c222715460e
Vora Parthiv <parthivvora123@gmail.com>
2024-02-03 14:14:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/securityapp/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.securityapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.securityapp.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
